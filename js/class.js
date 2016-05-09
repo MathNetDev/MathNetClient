5b33d3ed8fcf6bc1acbace380ea8ed95663d73e5
Anton Ochs <asochs@ucdavis.edu>
2016-05-09 14:43:44 -0700

    now only applies group limit to geogebra html users

--- js/class.js
+++ js/class.js
@@ -1,5 +1,7 @@
 "use strict";
 $(function() {
+    var geogebra = $("#ging");
+    var max_people = 2;
     var $logout_button = $('#logout');
 
     $logout_button.bind('click', function() {
@@ -12,8 +14,11 @@ $(function() {
     $('#buttons').bind('click', function(event) {
         var group_num = $(event.target).index('#buttons :input') + 1;
         var btn_str = $("#grp" + group_num).val()
-        var num_people = btn_str.substring(btn_str.lastIndexOf(" ") + 1); 
-        if (num_people < 2){
+        var num_people = btn_str.substring(btn_str.lastIndexOf(" ") + 1);
+        if(geogebra.length == 0){
+          max_people = Number.MAX_VALUE;
+        }
+        if (num_people < max_people){
           socket.group_join(sessionStorage.getItem('username'), 
                             sessionStorage.getItem('class_id'),
                             $(event.target).index('#buttons :input') + 1
