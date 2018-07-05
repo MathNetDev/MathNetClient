52686d72d46a89120ed948de076eed2cb85e528d
Jeroen Post <jjpost@ucdavis.edu>
2018-07-05 16:21:29 -0700

    Changing host file, testing

--- js/host.js
+++ js/host.js
@@ -1,1 +1,8 @@
-var host = "http://localhost:8889";
+var host = "/node";
+
+// Include the file in the root dir, this file can override the host on different environments
+// 8889 for live
+// 8888 for test
+// 8887 for dev
+
+require('../host.js');
