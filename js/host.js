c83d4aed3654d3bd7ea4bee6273daf3cc9a5118f
Jeroen Post <jjpost@ucdavis.edu>
2018-07-05 18:29:30 -0700

    Updated for all branches

--- js/host.js
+++ js/host.js
@@ -1,8 +1,1 @@
-var host = "/node";
-
-// Include the file in the root dir, this file can override the host on different environments
-// 8889 for live
-// 8888 for test
-// 8887 for dev
-
-require('../host.js');
+var host = window.location.hostname
