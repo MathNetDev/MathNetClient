b0743f21c4fd32cd810f2bd95440ccc0e7398c78
Dino <dinovin7@gmail.com>
2018-07-24 09:32:06 -0500

    Made some changes to the slider (shown in the student's webiste)

--- js/student_functions.js
+++ js/student_functions.js
@@ -257,7 +257,7 @@ function ggbOnInit(arg) {
     });
 }
 
-$step_size_slider.bind('change', function() {
+$step_size_slider.bind('mousemove', function() {
     stepSize = $step_size_slider.val()/10;
     $step_size_label.text($step_size_slider.val()/10);
 });
