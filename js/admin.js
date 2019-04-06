268c54dd8047e9048953d2935d78102e4a4e2ea5
Dino <dinovin7@gmail.com>
2019-04-05 20:46:12 -0500

    Fixing bug

--- js/admin.js
+++ js/admin.js
@@ -456,7 +456,7 @@ $(function() {
                             'coordSystem' : coordSystem
                         }
             };
-            admin_data_per_group[group_id] = data;
+            admin_data_per_group[data.group_id] = data;
             socket.xml_change(data);
         }
     });
